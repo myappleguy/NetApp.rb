10cb63f524d29b82b9772527ece14db77b5b2feb
Todd Pickell <tapickell@gmail.com>
2015-03-24 11:05:36 -0500

    spec runs with no examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
+require './lib/nmsdk/NaElement'
+require './lib/nmsdk/NaServer'
+require './lib/net-app-sdk/filer'
+require './lib/net-app-sdk/aggregate'
+require './lib/net-app-sdk/diag'
+require './lib/net-app-sdk/nfs'
+require './lib/net-app-sdk/qtree'
+require './lib/net-app-sdk/quota'
+require './lib/net-app-sdk/snapshot'
+require './lib/net-app-sdk/vfiler'
+require './lib/net-app-sdk/volume'
+require './lib/net-app-sdk/version'
+
 begin; require 'pry'; rescue LoadError; end
 
 require 'rspec'
