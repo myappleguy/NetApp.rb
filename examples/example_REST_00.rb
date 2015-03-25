8ca77539a5754a9a7b4b1a300023e77b8a0a0540
Todd Pickell <tapickell@gmail.com>
2015-03-25 11:13:45 -0500

    update to ad namespacing to the first example for testing purposes

--- examples/example_REST_00.rb
+++ examples/example_REST_00.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+#require this
 require 'sinatra'
 require 'json'
 #require 'bcrypt'
 require './lib/net-app-sdk'
 
+#these things I think need to be updated when we do our own based from this.
 @@fileraddr = "192.168.55.10"
 @@fileruser = "root"
 @@filerpwd = "rootr00t"
@@ -17,7 +19,7 @@ require './lib/net-app-sdk'
 
 def conn_filer
   # implement something generic over here
-  Filer.new(@@fileraddr, @@fileruser, @@filerpwd)
+  NetAppSdk::Filer.new(@@fileraddr, @@fileruser, @@filerpwd)
 end
 
 helpers do
@@ -33,51 +35,54 @@ helpers do
   end
 end
 
+#then write your route actions like this
+#POOF, instant Sinatra app
 get '/' do
   require "pry"; binding.pry
+  #this is where we are at in require "pry"; binding.pry
   "nope."
 end
 
 get '/aggregate' do
   protected!
   conn_filer
-  body(Aggregate.list).to_json
+  body(NetAppSdk::Aggregate.list).to_json
 end
 
 get '/volume' do
   protected!
   conn_filer
-  body(Volume.list).to_json
+  body(NetAppSdk::Volume.list).to_json
 end
 
 put '/volume' do
   protected!
   conn_filer
-  status 201 if Volume.create(params[:aggregate], params[:volumename], params[:size])
+  status 201 if NetAppSdk::Volume.create(params[:aggregate], params[:volumename], params[:size])
 end
 
 delete '/volume' do
   protected!
   conn_filer
-  status 200 if Volume.offline(params[:volumename]) and Volume.purge(params[:volumename])
+  status 200 if NetAppSdk::Volume.offline(params[:volumename]) and Volume.purge(params[:volumename])
 end
 
 get '/quota' do
   protected!
   conn_filer
-  body(Quota.list).to_json
+  body(NetAppSdk::Quota.list).to_json
 end
 
 put '/quota' do
   protected!
   conn_filer
   volumepath = "/vol/#{params[:volumename]}/"
-  status 201 if Quota.create(params[:volumename], volumepath, params[:quotasize], params[:type])
+  status 201 if NetAppSdk::Quota.create(params[:volumename], volumepath, params[:quotasize], params[:type])
 end
 
 delete '/quota' do
   protected!
   conn_filer
-  status 200 if Quota.purge(params[:quotaname], params[:volumename], params[:path], params[:type])
+  status 200 if NetAppSdk::Quota.purge(params[:quotaname], params[:volumename], params[:path], params[:type])
 end
 
